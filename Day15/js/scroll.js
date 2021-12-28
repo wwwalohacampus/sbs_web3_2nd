83359a685f83efdfd0123689e3451dabe3538ec9
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2021-12-28 16:57:41 +0900

    후앵

--- Day15/js/scroll.js
+++ Day15/js/scroll.js
@@ -1,62 +1,25 @@
 
 $(function() {
 
-    $(window).scroll(function(event) {
-        // 스크롤의 위치
-        let scrollTop = $(document).scrollTop()
-
-        // console.log("스크롤 위치 : " + scrollTop);
-
-        let titleLeft = $('.scroll-title').css('left')
-        let titleOffset = $('.scroll-title').offset()
-        // 타이틀의 left 속성의 값을 가져온다.
-        
-
-        
-        
-        // $('.scroll-title').css('left', titleOffset.left-=scrollTop*0.3)
-
-    })
-
-    // 스크롤 마우스 휠 방향 감지
-    $(window).bind('mousewheel', function(event) {
-        // 스크롤의 위치
-        // let scrollTop = $(document).scrollTop()
-        // let titleOffset = $('.scroll-title').offset()
-
-        // console.log("스크롤 위치 : " + scrollTop);
-        
-        // // 스크롤의 방향 (↑/↓)
-        // if( event.originalEvent.wheelDelta > 0 ){
-        //     console.log('위');
-        //     console.log("left " + titleOffset.left);
-
-        //     if( titleOffset.left < 0 ) {
-        //         $('.scroll-title').css('left', 0)
-        //     }
-        //     $('.scroll-title').css('left', titleOffset.left+=scrollTop*0.6)
-            
-        // } else {
-        //     console.log('아래');
-        //     console.log(titleOffset.left);
-        //     if( titleOffset.left > -600 )
-        //         $('.scroll-title').css('left', titleOffset.left-=scrollTop*0.6)
-        // }
-
-    })
-
-
-    // 스크롤의 방향 감지
-    let prev = 0        // 이전 스크롤 위치
+    /* 
+        스크롤의 방향 감지
+        - 이전 스크롤 위치
+        - 현재 스크롤 위치
+        - 이동 크기
+        - 최소 지점(left)
+        - 최대 지점(left)
+    */
+    let prev = $(this).scrollTop()          // 이전 스크롤 위치
     let move = 80
-    let leftMin = -1400
-    let leftMax = 1400
+    let leftMin = -1200
+    let leftMax = 1200
+
+    // 스크롤 이벤트
     $(window).on('scroll', function() {
         let now = $(this).scrollTop()       // 현재 스크롤 위치
-        
         let titleOffset = $('.scroll-title').offset()
-        console.log(now);
         
+        // scroll-title 인터렉션
         if( now > prev && titleOffset.left > leftMin && now > 0 && now < 1000 ){
             console.log('아래');
             // $('.scroll-title').css('left', titleOffset.left-=move)
@@ -67,8 +30,30 @@ $(function() {
             $('.scroll-title').animate({'left' : titleOffset.left+=move}, 5)
         }
 
-        prev = now
+        // 스크롤 방향에 따른 헤더 슬라이드
+        if( now > prev ) {
+            // 아래로 스크롤
+            $('header').slideUp()
+        } else if( now < prev ) {
+            // 위로 스크롤
+            $('header').slideDown()
+        }
 
+        console.log('now : ' + now);
+        // 따라 다니는 플로팅 버튼
+        $('.floating').stop().animate({'bottom': -now+15}, 300)
+
+        prev = now
     })
+
+    // 플로팅 버튼 클릭 이벤트
+    $('.floating').on('click', function() {
+
+        // 스크롤 맨 위로 이동
+        $('html').animate({'scroll-top' : 0}, 1000)
+        
+    })
+
+
 
 })
