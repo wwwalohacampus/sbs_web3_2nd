d668e45f8309f7ad2227c408f3dbad87c7dcefab
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2021-12-30 09:00:48 +0900

    17일차

---
+++ Day15/wow.js/js/script.js
@@ -0,0 +1,18 @@
+$(function() {
+    // wow.js 스크롤 애니메이션 옵셕 적용하기
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+    )
+
+
+    // wow.js 스크롤 애니메이션 시작하기!
+    wow.init();
+
+
+})
